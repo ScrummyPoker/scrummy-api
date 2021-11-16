d2ace0a06735bb4cf11a3d5adbf2ffdfd2bf55a9
Pedro Lima <pedrozin1234@gmail.com>
2021-11-16 11:28:50 -0300

    netlify fix

--- src/app.js
+++ src/app.js
@@ -38,13 +38,8 @@ app.use(mongoSanitize());
 app.use(compression());
 
 // enable cors
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', '*');
-  res.header('Access-Control-Allow-Headers', 'content-type');
-  app.use(cors());
-  next();
-});
+app.use(cors());
+app.options('*', cors());
 
 // jwt authentication
 app.use(passport.initialize());
