81dedf661e69bb22cf4ccd425fea4fd2ac55a400
Pedro Lima <pedrozin1234@gmail.com>
2021-11-16 11:23:35 -0300

    cors fixed headers return

--- src/app.js
+++ src/app.js
@@ -40,7 +40,8 @@ app.use(compression());
 // enable cors
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', 'content-type');
   app.use(cors());
   next();
 });
