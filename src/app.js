2dd7904dda7084821dd2edafc018d9930eb95863
Pedro Lima <pedrozin1234@gmail.com>
2021-11-16 10:50:07 -0300

    cors update to fix aplication

--- src/app.js
+++ src/app.js
@@ -38,7 +38,13 @@ app.use(mongoSanitize());
 app.use(compression());
 
 // enable cors
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  app.use(cors());
+  next();
+});
+
 app.options('*', cors());
 
 // jwt authentication
